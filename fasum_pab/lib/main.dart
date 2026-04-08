cedcf3ad7b0c684c3fa000977930a69f75d0db34
Justyn <justyncannavaro_2428250027@mhs.mdp.ac.id>
2026-04-08 10:45:21 +0700

    push

--- fasum_pab/lib/main.dart
+++ fasum_pab/lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum_pab/firebase_options.dart';
+import 'package:fasum_pab/screens/home_screen.dart';
+import 'package:fasum_pab/screens/sign_in_screen.dart';
 
-
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            return HomeScreen();
+          } else {
+            return const SignInScreen();
+          }
+        },
       ),
     );
   }
