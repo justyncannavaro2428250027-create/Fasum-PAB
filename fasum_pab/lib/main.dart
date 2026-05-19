19d67602675ad698f07ce5cb396ac5c015847569
Justyn <justyncannavaro_2428250027@mhs.mdp.ac.id>
2026-05-19 09:33:43 +0700

    push

--- fasum_pab/lib/main.dart
+++ fasum_pab/lib/main.dart
@@ -1,10 +1,74 @@
 import 'package:fasum_pab/screens/splash_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fasum_pab/firebase_options.dart';
 import 'package:fasum_pab/screens/home_screen.dart';
 import 'package:fasum_pab/screens/sign_in_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLovalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('Izin notifikasi diberikan');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('Izin notifikasi sementara diberikan');
+  } else {
+    print('Izin notifikasi ditolak');
+  }
+}
+
+Future<void> showBasicNotification(String? title, String? body) async {
+  final android = AndroidNotificationDetails(
+    'default_channel',
+    'Notifikasi Default',
+    channelDescription: 'Notifikasi masuk dari FCM',
+    importance: Importance.high,
+    priority: Priority.high,
+    showWhen: true,
+  );
+  final platform = NotificationDetails(android: android);
+  await flutterLocalNotificationsPlugin.show(0, title, body, platform);
+}
+
+Future<void> showNotificationFromData(Map<String, dynamic> data) async {
+  final title = data['title'] ?? 'Pesan Baru';
+  final body = data['body'] ?? '';
+  final senderName = data['senderName'] ?? 'Pengirim tidak diketahui';
+  final time = data['sendAt'] ?? '';
+  final photoUrl = data['senderPhotoUrl'] ?? '';
+
+  ByteArrayAndroidBitmap? largeIconBitmap;
+  if (photoUrl.isNotEmpty) {
+    final base64 = await _networkImageToBase64(photoUrl);
+    if (base64 != null) {
+      largeIconBitmap = ByteArrayAndroidBitmap.fromBase64String(base64);
+    }
+  }
+
+  final styleInfo = 
+  largeIconBitmap != null
+  ? BigPictureStyleInformation(
+    largeIconBitmap,
+    contentTitle: title,
+    summaryText: '$body\n\nDari : $senderName - $time',
+    largeIcon: largeIconBitmap,
+    hideExpandedLargeIcon: true
+  )
+  : BigTextStyleInformation(
+    '$body\n\nDari: $senderName\nWaktu: $time',
+    contentTitle: title
+  );
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
